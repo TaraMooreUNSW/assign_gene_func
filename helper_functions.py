b44db422783a4b25a03f52a2f9ea5ee68a43546d
TaraMooreUNSW <z5487977@ad.unsw.edu.au>
2026-09-21 21:37:52 +1000

    started working on implementing needleman-wunsch algorithm with gap penalty of 1, completed the scores for the matrix

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,9 @@
+from Bio import Align
+from Bio.Align import substitution_matrices
+# matrix = substitution_matrices.load("BLOSUM62")
+
+GAP_PENALTY = 1
+
 def global_alignment(seq1, seq2, scoring_function):
     """Global sequence alignment using the Needleman–Wunsch algorithm.
 
@@ -28,7 +34,35 @@ def global_alignment(seq1, seq2, scoring_function):
     Other alignments are not possible.
 
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
+
+    rows = len(seq2) + 1
+    cols = len(seq1) + 1
+
+    # initialise matrix
+    alignment_matrix = [["" for _ in range(cols)] for _ in range(rows)]
+    alignment_matrix[0][0] = 0
+
+    # set matrix initial row
+    for i in range(rows):
+        alignment_matrix[i][0] = -i*GAP_PENALTY
+
+
+    # set matrix initial col
+    for j in range(cols):
+        alignment_matrix[0][j] = -j*GAP_PENALTY
+
+    # recurrence
+    for i in range(1, rows):
+        for j in range(1, cols):
+            recurr_one = alignment_matrix[i-1][j-1] + scoring_function(seq1[j-1], seq2[i-1])
+            recurr_two = alignment_matrix[i-1][j] - GAP_PENALTY
+            recurr_three = alignment_matrix[i][j-1] - GAP_PENALTY
+
+            alignment_matrix[i][j] = max(recurr_one, recurr_two, recurr_three)
+
+    # TO DO - add backtracking
+
 
 
 def local_alignment(seq1, seq2, scoring_function):
@@ -68,3 +102,9 @@ def local_alignment(seq1, seq2, scoring_function):
 def scoring_function_simple(aa_i,aa_j):
     score = [-1, 1][aa_i == aa_j]
     return (score)
+
+def main():
+    global_alignment('abracadabra', 'dabarakadara',lambda x, y: [-1, 1][x == y])
+
+if __name__ == '__main__':
+    main()
